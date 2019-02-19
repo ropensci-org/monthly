2728e2cc3ccbfe33b1ca4c04cf9ab88404cc7c70
Scott Chamberlain <myrmecocystus@gmail.com>
2019-02-19 09:08:49 -0800

    todays news

--- generate_pkg_news.R
+++ generate_pkg_news.R
@@ -1,11 +1,11 @@
 # e.g., full string
-# * A new version (`v0.8.0`) of `bold` is on CRAN - interface to Bold Systems (<http://www.boldsystems.org/>) API. See the [release notes](https://github.com/ropensci/bold/releases/tag/v0.8.0) for changes. Checkout the [vignette](https://cran.rstudio.com/web/packages/bold/vignettes/bold_vignette.html) to get started. {{ "bold" | image_cran }} {{ "bold" | image_github }} 
+# * A new version (`v0.8.0`) of `bold` is on CRAN - interface to Bold Systems (<http://www.boldsystems.org/>) API. See the [release notes](https://github.com/ropensci/bold/releases/tag/v0.8.0) for changes. Checkout the [vignette](https://cran.rstudio.com/web/packages/bold/vignettes/bold_vignette.html) to get started. {{ "bold" | image_cran }} {{ "bold" | image_github }}
 # > `bold_identify_parents()` improvements, and `bold_specimens()` bug fix
 library(glue)
 library(dplyr)
 library(tidyselect)
 
-# template <- '* A new version (`v{ver}`) of `{pkg}` is on CRAN - {pkg_brief_description}. See the [release notes](https://github.com/{owner}/{pkg}/releases/tag/v{ver}) for changes. Checkout the [{docs_name}]({docs_url}) to get started. {{ "{pkg}" | image_cran }} {{ "{pkg}" | image_github }} 
+# template <- '* A new version (`v{ver}`) of `{pkg}` is on CRAN - {pkg_brief_description}. See the [release notes](https://github.com/{owner}/{pkg}/releases/tag/v{ver}) for changes. Checkout the [{docs_name}]({docs_url}) to get started. {{ "{pkg}" | image_cran }} {{ "{pkg}" | image_github }}
 # > xxx'
 # template <- '* A new version (`v{ver}`) of `{pkg}` is on CRAN - {pkg_brief_description}. See the [release notes]({release_url}) for changes. Checkout the [{docs_name}]({docs_url}) to get started. \\{\\{ "{pkg}" | image_cran \\}\\} \\{\\{ "{pkg}" | image_github \\}\\}
 # > xxx'
@@ -22,9 +22,9 @@ dat_new_vers <- readr::read_csv("data/newversions.csv")
 
 metad <- readr::read_csv("data/pkg_metadata.csv")
 
-tmp <- dat_new_vers %>%
 # tmp <- dat_new_pkgs %>%
-  left_join(metad) %>% 
+tmp <- dat_new_vers %>%
+  left_join(metad) %>%
   mutate(release_url = as.character(glue(template_release)))
 
 release_urls <- unlist(unname(Map(function(a, b, c) {
@@ -35,12 +35,14 @@ release_urls <- unlist(unname(Map(function(a, b, c) {
 }, tmp$release_url, tmp$owner, tmp$pkg)))
 tmp$release_url <- release_urls
 
-# tmp %>% 
-#   mutate(description = glue(template)) %>% 
+# tmp %>%
+#   mutate(description = glue(template)) %>%
 #   .$description
 # copy from the above
 
-tmp %>% 
-  mutate(description = sprintf(template_sprintf, ver, pkg, pkg_brief_description, release_url, docs_name, docs_url, pkg, pkg)) %>% 
-  .$description %>% 
+tmp %>%
+  mutate(description =
+    sprintf(template_sprintf, ver, pkg, pkg_brief_description,
+        release_url, docs_name, docs_url, pkg, pkg)) %>%
+  .$description %>%
   cat(sep = "\n")
